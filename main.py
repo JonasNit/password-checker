0bcc3014a8bf3d7e22f9003ce3fc931edbbcec15
JonasNit <jonasnitzsche@gmail.com>
2026-03-04 11:57:59 +0100

    Basis steht

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import random
+
+passwordLength = int(input("Wie lange muss das Passwort sein?"))
+characters = input("Welche Zeichen sollen benutzt werden?")
+passwords = int(input("Wie viel Passwörter willst du haben?"))
+
+for i in range(passwords):
+    password = ""
+    
+    for j in range(passwordLength):
+        randomCharacter = random.choice(characters)
+        password = password + randomCharacter
+    print(i + 1, ".", password)
+    
+
